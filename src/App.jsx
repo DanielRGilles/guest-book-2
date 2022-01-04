ddddc9736cb912c638963cfff0de5ac345918075
DanielRGilles <danryangilles@gmail.com>
2022-01-03 16:41:24 -0800

    added react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,52 @@
-import Entries from "./components/Entries";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Input from "./components/Input";
-import './App.css'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { PrivateRoute } from './components/PrivateRoute';
+import GuestBook from "./views/GuestBook";
+import Login from './views/Login'
 
 export default function App() {
   return (
-  <div className="w-2/3 h-screen bg-slate-500 justify-items-center">
-    <Header/>
-    <div className="cnt flex">
-    <div></div>
-    <main className="flex-col justify-self-center items-center w-2/3">
-    <Input/>
-    <Entries/>
-    <Footer/>  
-    </main>
-    <div></div>
-    </div>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/guest-book">Guest Book</Link>
+            </li>
+          </ul>
+        </nav>
 
-    )
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <PrivateRoute path="/guest-book">
+            <GuestBook />
+          </PrivateRoute>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
